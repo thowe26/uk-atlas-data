2d4fb0d87b740d8e07ddb5b36a4a744f218251bf
Tim <thowe26@googlemail.com>
2026-02-08 16:34:30 +0000

    Update atlas.js

--- atlas.js
+++ atlas.js
@@ -22,7 +22,7 @@ const historicalContext = {
         },
         finance: { 
             type: 'box', xMin: 2008, xMax: 2009, backgroundColor: 'rgba(50, 50, 50, 0.2)', borderWidth: 0, 
-            label: { content: ['Financial', 'Crisis'], display: true, position: { x: 'center', y: 'start' }, textAlign: 'center', yAdjust: 0, color: '#555', font: { size: 10, weight: 'normal' } } 
+            label: { content: ['2008', 'Financial', 'Crisis'], display: true, position: { x: 'center', y: 'start' }, textAlign: 'center', yAdjust: 0, color: '#555', font: { size: 10, weight: 'normal' } } 
         },
         covid: { 
             type: 'box', xMin: 2020, xMax: 2022, backgroundColor: 'rgba(50, 50, 50, 0.2)', borderWidth: 0, 
